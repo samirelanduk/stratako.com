eab345da4940dd5a79b2f6a163d69cce047163b6
Sam Ireland <sam.ireland.uk@gmail.com>
2020-11-28 16:50:16 +0000

    Operations page operations

--- src/pages/OperationsPage.jsx
+++ src/pages/OperationsPage.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
 import Base from "./Base";
+import { FUTURE_OPERATIONS } from "../queries";
 
 const OperationsPage = () => {
+
+  const { loading, data } = useQuery(FUTURE_OPERATIONS);
+
+  if (loading) {
+    return (
+      <Base className="operations-page">
+        Loading
+      </Base>
+    );
+  }
+
+  const operations = data.operations.edges.map(edge => edge.node);
+  const slot1 = operations.filter(operation => operation.slot === 1);
+  const slot2 = operations.filter(operation => operation.slot === 2);
+  const noSlot = operations.filter(operation => operation.slot === null);
+
   return (
     <Base className="operations-page">
-      Operations
+      <div className="slots">
+        <div className="slot">
+          <h2 className="slot-title">Slot 1</h2>
+          {slot1.map(operation => (
+            <div className="operation">
+              <h3>{operation.name}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="slot">
+          <h2 className="slot-title">Slot 2</h2>
+          {slot2.map(operation => (
+            <div className="operation">
+              <h3>{operation.name}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="slot">
+          <h2 className="slot-title">No Slot</h2>
+          {noSlot.map(operation => (
+            <div className="operation">
+              <h3>{operation.name}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </Base>
   );
 };
--- src/queries.js
+++ src/queries.js
@@ -5,3 +5,9 @@ export const CURRENT_OPERATIONS = gql`{
     edges { node { id name description slot } }
   }
 }`;
+
+export const FUTURE_OPERATIONS = gql`{
+  operations(started: false) {
+    edges { node { id name description slot } }
+  }
+}`;
