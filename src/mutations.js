1e71c9b41a39e947f43af65d9d3749e06625d1b1
Sam Ireland <sam.ireland.uk@gmail.com>
2020-11-29 00:02:24 +0000

    Operation completion

--- src/mutations.js
+++ src/mutations.js
@@ -1,5 +1,9 @@
 import gql from "graphql-tag";
 
+export const COMPLETE_OPERATION = gql`mutation complete($id: ID!) {
+  completeOperation(id: $id) { operation { id } }
+}`;
+
 export const REORDER_OPERATIONS = gql`mutation reorderOperations(
   $slot: ID! $operation: ID! $index: Int!
 ) { reorderOperations(slot: $slot operation: $operation index: $index) { slot {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import Base from "./Base";
 import { CURRENT_OPERATIONS } from "../queries";
+import { COMPLETE_OPERATION } from "../mutations";
 
 const HomePage = () => {
 
-  const { loading, data } = useQuery(CURRENT_OPERATIONS);
+  const { loading, data, refetch } = useQuery(CURRENT_OPERATIONS);
+
+  const [completeOperation, completeOperationMutation] = useMutation(COMPLETE_OPERATION, {
+    refetchQueries: [{query: CURRENT_OPERATIONS}]
+  });
+
+  const complete = id => {
+    completeOperation({
+      variables: {id}
+    })
+  }
 
   if (loading) {
     return (
@@ -23,6 +34,7 @@ const HomePage = () => {
             <h2 className="slot-title">{slot.name}</h2>
             {slot.operation && <div className="operation">
               <h3>{slot.operation.name}</h3>
+              <button onClick={() => complete(slot.operation.id)}>Complete</button>
             </div>}
           </div>
         ))}
