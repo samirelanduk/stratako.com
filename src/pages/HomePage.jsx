0e9f401b16464babd7cb8b60996e45d8f7caad0c
Sam Ireland <sam.ireland.uk@gmail.com>
2020-12-06 01:45:28 +0000

    Remove lag in task reorder

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   const { loading, data } = useQuery(CURRENT_OPERATIONS);
 
   const [moveTask,] = useMutation(MOVE_TASK, {
-    refetchQueries: [{query: CURRENT_OPERATIONS}],
+    //refetchQueries: [{query: CURRENT_OPERATIONS}],
   });
 
   if (loading) return <Base className="home-page" loading={true} />
@@ -23,9 +23,35 @@ const HomePage = () => {
       slot => slot.operation && slot.operation.id === sourceOperationId
     )[0].operation.tasks[result.source.index].id;
     const index = result.destination.index;
+    const variables = {id: taskId, index: index};
+    if (sourceOperationId !== destinationOperationId)
+      variables.operation = destinationOperationId;
     moveTask({
-      variables: {id: taskId, index: index, operation: destinationOperationId},
-    })
+      variables,
+      optimisticResponse: {
+        __typename: "Mutation",
+      },
+      update: (proxy) => {
+        const newData = JSON.parse(JSON.stringify(proxy.readQuery({ query: CURRENT_OPERATIONS})));        
+        for (let slot of newData.slots) {
+          if (slot.operation) {
+            for (let task of slot.operation.tasks) {
+              if (task.id === taskId) {
+                slot.operation.tasks = slot.operation.tasks.filter(task => task.id !== taskId);
+                for (let slot2 of newData.slots) {
+                  if (slot2.operation && slot2.operation.id === destinationOperationId) {
+                    slot2.operation.tasks.splice(index, 0, task);
+                    break;
+                  }
+                }
+                break;
+              }
+            }
+          }
+        }
+        proxy.writeQuery({ query: CURRENT_OPERATIONS, data: newData});
+      }
+    });
   }
 
   return (
