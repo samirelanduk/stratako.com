1b0eb051bf4cadc11ab9e9b7f1a8dd11633df735
Sam Ireland <sam.ireland.uk@gmail.com>
2020-12-19 23:10:00 +0000

    Reset again

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,9 @@
 import React from "react";
-import { Route } from "react-router";
-import { BrowserRouter, Switch } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client";
-import HomePage from "../pages/HomePage";
-import { makeClient } from "../api";
-import OperationsPage from "../pages/OperationsPage";
-import OperationPage from "../pages/OperationPage";
-import ProjectPage from "../pages/ProjectPage";
-import ProjectsPage from "../pages/ProjectsPage";
-
-const client = makeClient();
 
 const App = () => {
 
   return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/operations/" exact>
-            <OperationsPage />
-          </Route>
-          <Route path="/operations/:id" exact>
-            <OperationPage />
-          </Route>
-          <Route path="/projects/" exact>
-            <ProjectsPage />
-          </Route>
-          <Route path="/projects/:id" exact>
-            <ProjectPage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </ApolloProvider>
+    "stratako"
   )
 }
 
