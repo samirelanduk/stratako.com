453f6a07c557927aa7d28a33412c67c35c92bd73
Sam Ireland <sam.ireland.uk@gmail.com>
2021-05-08 00:18:06 +0100

    Use env variable for API location

--- src/api.js
+++ src/api.js
@@ -1,34 +1,16 @@
 import { ApolloClient, InMemoryCache, ApolloLink, createHttpLink } from "@apollo/client";
 import { TOKEN } from "./queries";
 
-export const isDevelopment = () => {
-  /**
-   * Returns true if app is running locally.
-   */
-  return !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-}
-
-
-export const getApiLocation = () => {
-  /**
-   * Gets the URL of the API.
-   */
-
-  if (isDevelopment()) {
-    return "http://localhost:8003/graphql";
-  } else {
-    return "https://api.stratako.com/graphql";
-  }
-}
-
-
 export const makeClient = () => {
   /**
    * Creates an apollo client with automatic token insertion, and catchall
    * error handling.
    */
 
-  const httpLink = createHttpLink({uri: getApiLocation(), credentials: "include"});
+  const httpLink = createHttpLink({
+    uri: `${process.env.REACT_APP_BACKEND}/graphql`,
+    credentials: "include"
+  });
 
   const authLink = new ApolloLink((operation, forward) => {
     const { cache } = operation.getContext();
