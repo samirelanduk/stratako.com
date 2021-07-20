043615defd91a9051a9f5138a26d0cd28e303653
Sam Ireland <sam.ireland.uk@gmail.com>
2021-07-20 12:08:52 +0100

    Reset. Again.

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return <div>stratako</div>
+}
+
+App.propTypes = {
+    
+};
+
+export default App;
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const PROJECT_STATUSES = {
-  1: "Active",
-  2: "Maintenance",
-  3: "On Hold",
-  4: "Not Started",
-  5: "Abandoned",
-  6: "Completed"
-}
